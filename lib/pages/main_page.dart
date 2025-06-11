487d39a996364857cb18b377ba6a197f470b8a05
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-06-11 14:09:38 +0700

    transaksi

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:uangku_app/pages/categoty_page.dart';
 import 'package:uangku_app/pages/home_page.dart';
+import 'package:uangku_app/pages/transaction_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -52,7 +53,15 @@ class _MainPageState extends State<MainPage> {
       floatingActionButton: Visibility(
         visible: (currentIndex == 0) ? true : false,
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.of(context)
+                .push(
+                  MaterialPageRoute(builder: (context) => TransactionPage()),
+                )
+                .then((value) {
+                  setState(() {});
+                });
+          },
           backgroundColor: Colors.green,
           child: Icon(Icons.add),
         ),
